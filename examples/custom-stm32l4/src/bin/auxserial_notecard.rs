5246a62c61b3b5d0633492582b86f67b2f7f5ebf
Piotr Esden-Tempski <piotr@esden.net>
2025-03-28 13:28:35 -0700

    ex: Initial commit of example projects.

---
+++ examples/custom-stm32l4/src/bin/auxserial_notecard.rs
@@ -0,0 +1,80 @@
+#![no_std]
+#![no_main]
+
+use blues_notecard_next::Notecard;
+
+use defmt::*;
+use {defmt_rtt as _, panic_probe as _};
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::usart::{self, BufferedUart};
+use embassy_stm32::{bind_interrupts, peripherals};
+use embassy_time::{Delay, Instant, Timer};
+
+bind_interrupts!(struct NoteIrqs {
+    USART2 => usart::BufferedInterruptHandler<peripherals::USART2>;
+});
+
+// static DEBUG_UART: StaticCell<usart::Uart<mode::Blocking>> = StaticCell::new();
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    // Configure and initialize system clock tree
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.sys = Sysclk::PLL1_R;
+        config.rcc.hsi = true;
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV1,
+            mul: PllMul::MUL20,
+            divp: None,
+            divq: None,
+            divr: Some(PllRDiv::DIV4),
+        });
+        config.rcc.ls = LsConfig::default_lse();
+        config.rcc.mux.usart1sel = mux::Usart1sel::SYS;
+    }
+    let p = embassy_stm32::init(config);
+
+    info!("Hello World!");
+
+    let mut config = usart::Config::default();
+    config.baudrate = 115200;
+    let mut tx_buffer = [0_u8; 256];
+    let mut rx_buffer = [0_u8; 256];
+    let usart = BufferedUart::new(
+        p.USART2,
+        NoteIrqs,
+        p.PA3,
+        p.PA2,
+        &mut tx_buffer,
+        &mut rx_buffer,
+        config,
+    )
+    .unwrap();
+
+    // Enable aux serial
+    let _aux_en = Output::new(p.PB2, Level::High, Speed::Low);
+    Timer::after_millis(100).await;
+
+    struct EmbassyClock;
+    impl blues_notecard_next::Now for EmbassyClock {
+        fn now_micros(&self) -> u64 {
+            Instant::now().as_micros()
+        }
+    }
+
+    let clock = EmbassyClock;
+    let delay = Delay;
+
+    // Configure notecard
+    let mut note = Notecard::new(usart, clock, delay);
+
+    let r = note.transaction().await;
+
+    info!("done {}", r);
+
+    loop {}
+}
---
+++ examples/custom-stm32l4/src/bin/auxserial_raw_ping.rs
@@ -0,0 +1,85 @@
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+
+use defmt::*;
+use {defmt_rtt as _, panic_probe as _};
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::usart::{self, Uart};
+use embassy_stm32::{bind_interrupts, peripherals};
+use embassy_time::Timer;
+use heapless::String;
+
+bind_interrupts!(struct NoteIrqs {
+    USART2 => usart::InterruptHandler<peripherals::USART2>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    // Configure and initialize system clock tree
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.sys = Sysclk::PLL1_R;
+        config.rcc.hsi = true;
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV1,
+            mul: PllMul::MUL20,
+            divp: None,
+            divq: None,
+            divr: Some(PllRDiv::DIV4),
+        });
+        config.rcc.ls = LsConfig::default_lse();
+        config.rcc.mux.usart1sel = mux::Usart1sel::SYS;
+    }
+    let p = embassy_stm32::init(config);
+
+    info!("Hello World!");
+
+    let mut config = usart::Config::default();
+    config.baudrate = 115200;
+    let mut usart = Uart::new(
+        p.USART2, p.PA3, p.PA2, NoteIrqs, p.DMA1_CH7, p.DMA1_CH6, config,
+    )
+    .unwrap();
+
+    let _aux_en = Output::new(p.PB2, Level::High, Speed::Low);
+    Timer::after_millis(100).await;
+
+    for _n in 0u32.. {
+        let mut s: String<128> = String::new();
+        core::write!(&mut s, "\n").unwrap();
+
+        info!("Writing...");
+        usart.write(s.as_bytes()).await.ok();
+
+        info!("wrote DMA");
+        info!("Reading...");
+        let mut buffer = [0_u8; 256];
+        let r = usart.read_until_idle(&mut buffer).await.ok();
+        if let Some(len) = r {
+            info!(
+                "Received {} bytes: \"{:?}\"",
+                len,
+                core::str::from_utf8(&buffer[0..len]).unwrap()
+            );
+        } else {
+            info!("Did not receive anything...");
+        }
+
+        Timer::after_millis(1000).await;
+    }
+}
+
+// #[inline(never)]
+// #[panic_handler]
+// #[allow(dead_code)]
+// fn panic(info: &PanicInfo) -> ! {
+//     error!("panic: {}", defmt::Debug2Format(info));
+//     error!("halting...");
+//     defmt::flush();
+//     loop {}
+// }
